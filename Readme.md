c13e8acbd23d7a85a38f26ff41d1827736290f1d
Bwatt <pierrickboitel.utc@gmail.com>
2017-04-09 17:59:36 +0200

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Description
+
+Script to send and email when the forecast temperature will be under a number (to get your plant inside for example).
+
+# Usage
+* Get an API key on open weather map
+* Complete the config.json file by your informations
+* Install the virtualenvironment
+* Use a cron to launch your script every day
---
+++ test_mail.py
@@ -0,0 +1,58 @@
+# coding: utf-8
+
+import json
+import smtplib
+import sys
+import os
+import datetime
+
+import requests
+
+def send_mail(config, mail_to, message):
+    server = smtplib.SMTP(config["server_mail"],
+                          config["port_server_mail"])
+    server.starttls()
+    server.login(config["username"], config["password"])
+    server.sendmail(config["email_from"], mail_to, message)
+    server.quit()
+
+
+def create_mail(config, mail_to, temp_min):
+
+    temp_min = float(temp_min) - 273.15
+    message = config["message"].replace("####", str(temp_min))
+    mail = ("From: %s\nTo: %s\nSubject: %s\n\n\n%s" %
+           (config["email_from"], mail_to, config["subject"], message))
+    return mail
+
+
+def get_tomorow_date(data):
+    now = datetime.datetime.now()
+    delta = datetime.datetime.strptime(data, "%Y-%m-%d %H:%M:%S") - now
+    if delta.days == 0:
+        return True
+    return False
+
+# Location of the python file
+dir_name = os.path.dirname(__file__)
+
+# Load of the config
+with open(os.path.join(dir_name, 'config.json')) as my_json:
+    config = json.load(my_json)
+
+
+# send_mail(config, message)
+response = requests.get("{url}?q={city}&APPID={key}".format(
+                            url=config["owm_url"], city=config["city"],
+                            key=config["owm_api_key"]))
+weather_json = response.json()
+
+now = datetime.datetime.now()
+tomorow_data = [data for data in weather_json["list"]
+                if get_tomorow_date(data["dt_txt"])]
+for data in tomorow_data:
+    if float(data["main"]["temp_min"]) < float(config["temp_min"]):
+        for mail_to in config["mail_to"]:
+            mail = create_mail(config, mail_to, data["main"]["temp_min"])
+            send_mail(config, mail_to, mail)
+        break
